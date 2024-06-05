208a527903e3dd9e7fab41ae3228dd3985b5d3a8
limaenz <enzolimafranca@gmail.com>
2024-06-04 22:14:04 -0300

    finish the game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::{io, u32};
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let numero_secreto = rand::thread_rng()
+        .gen_range(1, 100);
+    
+    println!("Adivinhe o número!");
+
+    loop {
+        let mut palpite = String::new();
+        
+        println!("Digite o seu palpite.");
+        
+        io::stdin().read_line(&mut palpite)
+            .expect("Falha ao ler entrada");    
+
+        let palpite: u32 = match palpite.trim()
+        .parse() {
+            Ok(numero) => numero,
+            Err(_) => continue
+        };
+
+        println!("Você disse: {}", palpite);
+
+        match palpite.cmp(&numero_secreto) {
+            std::cmp::Ordering::Greater => println!("MUITO ALTO!!!"),
+            std::cmp::Ordering::Less => println!("MUITO BAIXO!!!"),
+            std::cmp::Ordering::Equal => {
+                println!("ACERTOU AMIGÃO!!!");
+                break;            
+            }
+        }
+    }
 }
